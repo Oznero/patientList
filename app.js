e7244ebc1febd312410a4efe076bfb6a39699902
oznero <kcroom@gmail.com>
2018-10-14 17:59:19 -0400

    updated files

--- app.js
+++ app.js
@@ -15,19 +15,40 @@ while (keepAlive != false) {
     if (command.toLocaleLowerCase() === 'add') {
       let addedName = prompt('Please give us a new name to add to the list');
       patientList.push(addedName);
+      for (let x = 1; x < patientList.length; x++) {
+        patientList[x] = patientList[x].charAt(0).toUpperCase() + patientList[x].substr(1);
+      }
       alert(patientList);
-      keepAlive = confirm('Do you have anymore names you would like to add?');
+      keepAlive = confirm('Taking you back to the main menu.');
     }
 
+    /*
     if (command.toLocaleLowerCase() === 'update') {
-      updateOrder(keepAlive);
+      let updateName = prompt('Please give us a patient name to update' + patientList);
+      for (let i = 0; i < patientList; i++) {
+        if (updateName.toLocaleLowerCase() === patientList[i]) {
+          alert('We updated the list' + patientList);
+          keepAlive = confirm('Do you have anymore names you would like to update?');
+        }
+        else {
+          alert('We did not find ' + updateName + 'in the list');
+        }
+      }
+    }
+*/
+    if (command.toLocaleLowerCase() === 'delete') {
+      let delUser = prompt('Please type a name you would like to remove' + patientList).replace(/^\w/, c => c.toUpperCase());
+      let index = patientList.indexOf(delUser)
+      if (index !== -1) {
+        patientList.splice(index, 1);
+      }
+      alert(patientList);
+      keepAlive = confirm('Taking you back to the main menu.');
     }
 
-    if (command.toLocaleLowerCase() === 'delete') {
-      deleteName(keepAlive);
-    }
     if (command.toLocaleLowerCase() === 'reorder') {
-      reorderList(keepAlive);
+      let firstUser = prompt('Please give us the first person').replace(/^\w/, c => c.toUpperCase());
+      let secondUser = prompt('Please give us the second person').replace(/^\w/, c => c.toUpperCase());
     }
   }
   else {
