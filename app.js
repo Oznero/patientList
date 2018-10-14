fe7d39b68595a493db0e2da02d3150a6d60980b3
oznero <kcroom@gmail.com>
2018-10-14 16:51:03 -0400

    updated files

--- app.js
+++ app.js
@@ -5,43 +5,69 @@ const patientList = [
   'Maryam'
 ]
 
-alert(patientList);
+//alert(patientList);
 
-const command = prompt('Choose one: update, delete, add, reorder');
+let keepAlive = true;
 
+while (keepAlive != false) {
+  const command = prompt('Choose one: update, delete, add, reorder');
+  if (command.toLocaleLowerCase() == 'update' || command.toLocaleLowerCase() == 'delete' || command.toLocaleLowerCase() == 'add' || command.toLocaleLowerCase() == 'reorder') {
+    if (command.toLocaleLowerCase() === 'add') {
+      let addedName = prompt('Please give us a new name to add to the list');
+      patientList.push(addedName);
+      alert(patientList);
+      keepAlive = confirm('Do you have anymore names you would like to add?');
+    }
 
-if(command.toLocaleLowerCase() == 'update' || command.toLocaleLowerCase() == 'delete' || command.toLocaleLowerCase() == 'add' || command.toLocaleLowerCase() == 'reorder'){
+    if (command.toLocaleLowerCase() === 'update') {
+      updateOrder(keepAlive);
+    }
 
-  // *** Here we add a name to array patientList
-  if(command.toLocaleLowerCase() === 'add'){
-    let addedName = prompt('Please give us a new name to add to the list');
-    patientList.push(addedName);
-    document.write(patientList);
-  }
-
-  // *** Here we update a name in array patientList
-  if(command.toLocaleLowerCase() === 'update'){
-    let updateName = prompt('Please give us a patient name to update' + patientList);
-    for(let i = 0; i < patientList; i++){
-      if(updateName.toLocaleLowerCase() === patientList[i]){
-        alert('We updated the list');
-      }
-      else{
-        alert('We did not find ' + updateName + 'in the list');
-      }
+    if (command.toLocaleLowerCase() === 'delete') {
+      deleteName(keepAlive);
+    }
+    if (command.toLocaleLowerCase() === 'reorder') {
+      reorderList(keepAlive);
     }
   }
-
-  
-  if(command.toLocaleLowerCase() === 'delete'){
-
-  }
-  if(command.toLocaleLowerCase() === 'reorder'){
-
+  else {
+    confirm('We did not recognize your option please try again');
   }
 }
 
-else{
-  confirm('We did not recognize your option please refresh and try again');
-  alert(command);
+/*
+if (keepAlive === 1) {
+  prompt(command);
+  // *** Here we add a name to array patientList
+  if (command.toLocaleLowerCase() == 'update' || command.toLocaleLowerCase() == 'delete' || command.toLocaleLowerCase() == 'add' || command.toLocaleLowerCase() == 'reorder') {
+    if (command.toLocaleLowerCase() === 'add') {
+      addName();
+    }
+  }
 }
+*/
+
+
+/*
+// *** Here we update a name in array patientList
+if (command.toLocaleLowerCase() === 'update') {
+  let updateName = prompt('Please give us a patient name to update' + patientList);
+  for (let i = 0; i < patientList; i++) {
+    if (updateName.toLocaleLowerCase() === patientList[i]) {
+      alert('We updated the list');
+    }
+    else {
+      alert('We did not find ' + updateName + 'in the list');
+    }
+  }
+}
+
+
+if (command.toLocaleLowerCase() === 'delete') {
+
+}
+if (command.toLocaleLowerCase() === 'reorder') {
+
+}
+}
+*/
